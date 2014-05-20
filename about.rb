6a2cb038367b231913900b74bd41fcd4ef88dbe6
Romy Misra <romymisra@gmail.com>
2014-05-20 16:52:25 -0700

    thirteenth

--- about.rb
+++ about.rb
@@ -3,7 +3,7 @@ get '/about' do
     H.page {
         H.div(:id => "about") {
             H.h2 {"#{SiteName}"}+
-            H.p {"Welcome to the internal forum on how to update fetaure requests."}+
+            H.p {"Welcome to the internal forum on how to submit fetaure requests."}+
             H.p {"The goal is for people to be able to upvote and downvote internal features."}+
             
             H.p {"Creators of this are Larissa and Romy."}
