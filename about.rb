f27b63c880475736559304dc52d77dc5e890aedb
Larissa Russell <rissyrussell@gmail.com>
2014-05-20 16:58:41 -0700

    hi romy

--- about.rb
+++ about.rb
@@ -4,9 +4,7 @@ get '/about' do
         H.div(:id => "about") {
             H.h2 {"#{SiteName}"}+
             H.p {"Welcome to the internal forum on how to submit fetaure requests."}+
-        	H.p {""}
             H.p {"The goal is for us to be able to upvote internal features as well as make comments."}+
-            H.p {""}
             H.p {"Creators of this are Larissa and Romy."}
         }
     }
