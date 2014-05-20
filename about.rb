7a81ac0557d7996153b66b166a792a0ac3cf66b3
Larissa Russell <rissyrussell@gmail.com>
2014-05-20 16:56:11 -0700

    about section

--- about.rb
+++ about.rb
@@ -4,8 +4,9 @@ get '/about' do
         H.div(:id => "about") {
             H.h2 {"#{SiteName}"}+
             H.p {"Welcome to the internal forum on how to submit fetaure requests."}+
-            H.p {"The goal is for people to be able to upvote and downvote internal features."}+
-            
+        	H.p {""}
+            H.p {"The goal is for us to be able to upvote internal features as well as make comments."}+
+            H.p {""}
             H.p {"Creators of this are Larissa and Romy."}
         }
     }
