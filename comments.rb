1a686cd47c8cb55147a496cb280a0f844ccf0a6c
Romy Misra <romymisra@gmail.com>
2014-05-20 17:26:35 -0700

    seventeen

--- comments.rb
+++ comments.rb
@@ -27,18 +27,14 @@
 
 require 'rubygems'
 require 'json'
-require 'RMagick'
+
 class PostsController < ApplicationController
-   http_basic_authenticate_with name: "dhh", password: "secret", except: :index
+   http_basic_authenticate_with name: "dhh", password: "secret"
 
    def index
-     render plain: "Everyone can see me!"
    end
 
-   def edit
-     render plain: "I'm only accessible if you know the password"
-   end
-end
+  end
 
 class RedisComments
     def initialize(redis,namespace,sort_proc=nil)
