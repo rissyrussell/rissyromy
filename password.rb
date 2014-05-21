eedb63965ad958fa50057dcb0101eacdbe29689c
Romy Misra <romymisra@gmail.com>
2014-05-20 17:16:54 -0700

    fifteenth

---
+++ password.rb
@@ -0,0 +1,13 @@
+require 'rubygems'
+require 'RMagick'
+class PostsController < ApplicationController
+   http_basic_authenticate_with name: "dhh", password: "secret", except: :index
+
+   def index
+     render plain: "Everyone can see me!"
+   end
+
+   def edit
+     render plain: "I'm only accessible if you know the password"
+   end
+end
