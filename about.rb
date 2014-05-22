f110288d2530e5c04f209c96067ab07caf787bc8
Larissa Russell <rissyrussell@gmail.com>
2014-05-22 16:49:44 -0700

    committing

--- about.rb
+++ about.rb
@@ -3,9 +3,9 @@ get '/about' do
     H.page {
         H.div(:id => "about") {
             H.h2 {"#{SiteName}"}+
-            H.p {"Welcome to the internal forum on how to submit fetaure requests."}+
-            H.p {"The goal is for us to be able to upvote internal features as well as make comments."}+
-            H.p {"Creators of this are Larissa and Romy."}
+            H.p {"Welcome to the internal forum for feature requests."}+
+            H.p {"The goal is for us to be able to effectively prioritize new features by upvoting and making comments."}+
+            H.p {"Creators of this page are Larissa and Romy :) ."}
         }
     }
 end
