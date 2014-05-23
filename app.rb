3ac4f0aafa80e51e644305e8f781dfd788c37750
Larissa Russell <rissyrussell@gmail.com>
2014-05-23 12:12:48 -0700

    hi

--- app.rb
+++ app.rb
@@ -44,6 +44,8 @@ require 'uri'
 
 Version = "0.11.0"
 
+ class PostsController < ApplicationController
+   http_basic_authenticate_with name: "dhh", password: "secret"
 
 def setup_redis(uri=RedisURL)
     uri = URI.parse(uri)
