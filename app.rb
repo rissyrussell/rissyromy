33a577e0be739ece61e95201dce0c9a28cd06693
Larissa Russell <rissyrussell@gmail.com>
2014-05-23 14:57:22 -0700

    lsdkjf

--- app.rb
+++ app.rb
@@ -44,7 +44,22 @@ require 'uri'
 
 Version = "0.11.0"
 
-http_basic_authenticate_with :name => "dhh", :password => "secret"
+#here is the beginning
+class ApplicationController < ActionController::Base
+USER, PASSWORD = 'dhh', 'secret'
+
+before_filter :authentication_check, :except => :index
+
+#Any other method
+
+ private
+  def authentication_check
+   authenticate_or_request_with_http_basic do |user, password|
+    user == USER && password == PASSWORD
+   end
+  end
+end
+#here is the end
 
 def setup_redis(uri=RedisURL)
     uri = URI.parse(uri)
