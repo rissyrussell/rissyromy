b3308f4184a1404eaa1f764f768aa472b73bd676
Larissa Russell <rissyrussell@gmail.com>
2014-05-23 12:13:53 -0700

    slkdfja

--- app.rb
+++ app.rb
@@ -44,9 +44,6 @@ require 'uri'
 
 Version = "0.11.0"
 
- class PostsController < ApplicationController
-   http_basic_authenticate_with name: "dhh", password: "secret"
-
 def setup_redis(uri=RedisURL)
     uri = URI.parse(uri)
     $r = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password) unless $r
