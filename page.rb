a014439f30c702117256ab811b39b47328bca475
Romy Misra <romymisra@gmail.com>
2014-05-24 15:14:32 -0700

    fortyseven

--- page.rb
+++ page.rb
@@ -146,12 +146,6 @@ class HTMLGen
     end
 end  
 
-class AdminController < ApplicationController
-  before_filter :authenticate
 
-  def authenticate
-    authenticate_or_request_with_http_basic('Administration') do |username, password|
-      username == 'admin' && password == 'password'
-    end
-  end
-  
+
+
