424a63e88b7c552c14d6ec9c31843b2082dae052
Romy Misra <romymisra@gmail.com>
2014-05-25 22:03:12 -0700

    nientynine

--- page.rb
+++ page.rb
@@ -131,8 +131,6 @@ class HTMLGen
                 self.meta(:content => "width=device-width, initial-scale=1, maximum-scale=1", :name => :viewport)+
                 self.link(:href => "/css/style.css?v=10", :rel => "stylesheet",
                           :type => "text/css")+
-                self.link(:href => "http://yui.yahooapis.com/pure/0.4.2/pure-min.css", :rel => "stylesheet",
-                          :type => "text/css")+
                 self.link(:href => "http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic", :rel => "stylesheet",
                           :type => "text/css")+
                 self.link(:href => "/images/visuallylogo.jpeg", :rel => "shortcut icon")+
