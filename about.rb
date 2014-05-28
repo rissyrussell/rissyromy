eed07a06009b7b5abf60939d357ea30f42805dfb
Romy Misra <romymisra@gmail.com>
2014-05-27 22:16:47 -0700

    hdredtwlve

--- about.rb
+++ about.rb
@@ -4,6 +4,7 @@ get '/about' do
         H.div(:id => "about") {
             H.h2 {"#{SiteName}"}+
             H.p {"Welcome to the internal forum for feature requests."}+
+            H.p {" "}+
             H.p {"The goal is for us to be able to effectively prioritize new features by making comments and upvoting ideas."}+
             H.p {"Creators of this page are Larissa and Romy :)"}
         }
