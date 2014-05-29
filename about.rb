6616527be2c56f907a50f8ca2c587c09ff8ec7bb
Larissa Russell <rissyrussell@gmail.com>
2014-05-29 13:01:34 -0700

    about pg

--- about.rb
+++ about.rb
@@ -5,7 +5,7 @@ get '/about' do
             H.h2 {"#{SiteName}"}+
             H.p {"Welcome to the internal forum for feature requests."}+
             H.p {"The goal is for us to be able to effectively prioritize new features by making comments and upvoting ideas."}+
-            H.p {"Here's how to add a new feature: Please cateogrize it as either website, community, marketplace, internal operations. In the description please clearly state the business problem you're proposing to solve."}+
+            H.p {"To add a new feature, please cateogrize it as either website, community, marketplace, or internal operations. Please also clearly state the business problem you're proposing to solve."}+
             H.p {"Max karma points get great prizes - happy submitting!"}+
             H.p {"Creators of this page are Larissa and Romy."}
         }
